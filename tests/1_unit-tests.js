e6e3ab9eca30e41e03eeedfdb4f240f8220d44b8
Promise Fru <promisefru1@gmail.com>
2020-12-11 13:16:35 +0100

    unit test updated

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -13,75 +13,92 @@ suite('Unit Tests', () => {
 
     suite('Translate American to British', () => {
 
-        test('Mangoes are my favorite fruit', (done) => {
-            var text = "Mangoes are my favorite fruit";
-            var translation = "Mangoes are my favourite fruit"
+        suite('American to British Func', () => {
 
-            //done();
-        });
+            test('Mangoes are my favorite fruit', (done) => {
+                var text = "Mangoes are my favorite fruit";
+                var translation = "Mangoes are my favourite fruit"
 
-        test('I ate yogurt for breakfast', (done) => {
-            var text = "I ate yogurt for breakfast";
-            var translation = "I ate yoghurt for breakfast"
+                //done();
+            });
 
-            //done();
-        });
+            test('I ate yogurt for breakfast', (done) => {
+                var text = "I ate yogurt for breakfast";
+                var translation = "I ate yoghurt for breakfast"
 
-        test("We had a party at my friend's condo", (done) => {
-            var text = "We had a party at my friend's condo";
-            var translation = "We had a party at my friend's flat"
+                //done();
+            });
 
-            //done();
-        });
+        })
 
-        test("Can you toss this in the trashcan for me?", (done) => {
-            var text = "Can you toss this in the trashcan for me?";
-            var translation = "Can you toss this in the bin for me?"
+        suite('american only func', () => {
 
-            //done();
-        });
+            test("We had a party at my friend's condo", (done) => {
+                var text = "We had a party at my friend's condo";
+                var translation = "We had a party at my friend's flat"
 
-        test("The parking lot was full", (done) => {
-            var text = "The parking lot was full";
-            var translation = "The car park was full"
+                //done();
+            });
 
-            //done();
-        });
+            test("Can you toss this in the trashcan for me?", (done) => {
+                var text = "Can you toss this in the trashcan for me?";
+                var translation = "Can you toss this in the bin for me?"
 
-        test("Like a high tech Rube Goldberg machine", (done) => {
-            var text = "Like a high tech Rube Goldberg machine";
-            var translation = "Like a high tech Heath Robinson device"
+                //done();
+            });
 
-            //done();
-        });
 
-        test("To play hooky means to skip class or work", (done) => {
-            var text = "To play hooky means to skip class or work";
-            var translation = "To bunk off means to skip class or work"
+            test("The parking lot was full", (done) => {
+                var text = "The parking lot was full";
+                var translation = "The car park was full"
 
-            //done();
-        });
+                //done();
+            });
 
-        test("No Mr. Bond, I expect you to die", (done) => {
-            var text = "No Mr. Bond, I expect you to die";
-            var translation = "No Mr Bond, I expect you to die"
+            test("Like a high tech Rube Goldberg machine", (done) => {
+                var text = "Like a high tech Rube Goldberg machine";
+                var translation = "Like a high tech Heath Robinson device"
 
-            //done();
-        });
+                //done();
+            });
 
-        test("Dr. Grosh will see you now", (done) => {
-            var text = "Dr. Grosh will see you now";
-            var translation = "Dr Grosh will see you now"
+            test("To play hooky means to skip class or work", (done) => {
+                var text = "To play hooky means to skip class or work";
+                var translation = "To bunk off means to skip class or work"
 
-            //done();
-        });
+                //done();
+            });
 
-        test("Lunch is at 12:15 today", (done) => {
-            var text = "Lunch is at 12:15 today";
-            var translation = "Lunch is at 12.15 today"
+        })
 
-            //done();
-        });
+        suite('american to british titles func', () => {
+
+            test("No Mr. Bond, I expect you to die", (done) => {
+                var text = "No Mr. Bond, I expect you to die";
+                var translation = "No Mr Bond, I expect you to die"
+
+                //done();
+            });
+
+            test("Dr. Grosh will see you now", (done) => {
+                var text = "Dr. Grosh will see you now";
+                var translation = "Dr Grosh will see you now"
+
+                //done();
+            });
+
+        })
+
+        suite('american to british time', () => {
+
+            test("Lunch is at 12:15 today", (done) => {
+                var text = "Lunch is at 12:15 today";
+                var translation = "Lunch is at 12.15 today"
+
+                //done();
+            });
+
+        })
 
     })
 
