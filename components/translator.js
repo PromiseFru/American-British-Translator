3c82f134a87aa3ec37bf7cc33f5c40d87270157d
Promise Fru <promisefru1@gmail.com>
2020-12-11 16:04:58 +0100

    americanToBritish created

--- components/translator.js
+++ components/translator.js
@@ -5,8 +5,29 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
+    americanToBritish(text) {
+        // get all american and british spellings and put in array
+        var americanSpellings = Object.keys(americanToBritishSpelling);
+        var britishSpellings = Object.values(americanToBritishSpelling);
+
+        // search text for any matching words or sentences in dic
+        for (var i = 0; i < americanSpellings.length; i++) {
+            var regex = new RegExp(`${americanSpellings[i]}`, "gi")
+            var searchText = text.match(regex);
+
+            if (searchText) {
+                text = text.replace(regex, britishSpellings[i]);
+            }
+
+        }
+
+        return text;
+    }
+
 }
 
-var input = "this bangs is a test acclimate accessorize"
+var input = "this bangs is a test acclimate accessorize aerogram"
+
+console.log(new Translator().americanToBritish(input))
 
 module.exports = Translator;
