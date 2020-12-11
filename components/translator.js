1b1770938da3fb9edd166394139fca50c3a11285
Promise Fru <promisefru1@gmail.com>
2020-12-11 12:15:18 +0100

    unit_test updated

--- components/translator.js
+++ components/translator.js
@@ -5,39 +5,8 @@ const britishOnly = require('./british-only.js')
 
 class Translator {
 
-    convert(text, dic) {
-        if (dic[text]) {
-            return dic[text];
-        }
-        return false
-    }
-
-    highlighter(text) {
-        return `<span class="highlight">${text}</span>`
-    }
-
-    americanToBritish(text) {
-        var oldWords = text.split(' ');
-        var newWords
-
-        oldWords.forEach((ele, i) => {
-            var AO = this.convert(ele, americanOnly)
-            var ATBS = this.convert(ele, americanToBritishSpelling)
-            var ATBT = this.convert(ele, americanToBritishTitles)
-
-            if (AO) oldWords.splice(i, 1, this.highlighter(AO))
-            if (ATBS) oldWords.splice(i, 1, this.highlighter(ATBS))
-            if (ATBT) oldWords.splice(i, 1, this.highlighter(ATBT))
-        })
-
-        newWords = oldWords.join(' ');
-
-        return newWords
-    }
 }
 
 var input = "this bangs is a test acclimate accessorize"
-
-console.log(new Translator().americanToBritish(input));
 
 module.exports = Translator;
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -2,7 +2,30 @@ const chai = require('chai');
 const assert = chai.assert;
 
 const Translator = require('../components/translator.js');
+var translate
 
 suite('Unit Tests', () => {
+
+    suiteSetup('setup', (done) => {
+        translate = new Translator()
+        done();
+    })
+
+    suite('Translate American to British', () => {
+
+        test('Mangoes are my favorite fruit', (done) => {
+            var text = "Mangoes are my favorite fruit";
+            var translation = "Mangoes are my favourite fruit"
+
+            //done();
+        });
+
+        test('I ate yogurt for breakfast', (done) => {
+            var text = "I ate yogurt for breakfast";
+            var translation = "I ate yoghurt for breakfast"
+
+            //done();
+        });
+    })
 
 });
